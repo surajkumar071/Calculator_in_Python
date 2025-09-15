69d633d70c34044ddb403588b3b4e507ab51a398
Suraj Kumar <surajk2072003@gmail.com>
2025-09-15 10:15:59 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+list =['add','sub','multiply','divide']
+c = input("Choose your operation : ")
+
+for i in list:
+    if (i == c):
+        if(i == 'add'):
+            print(a+b)
+            break
+        elif(i == 'sub'):
+            print(a-b)
+            break
+        elif(i == 'multiply'):
+            print(a*b)
+            break
+        else:
+            print(a/b)
+            break
+else:  print("Operation not found !")
