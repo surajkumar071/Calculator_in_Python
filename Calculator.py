12eb7bde1cbecf58c2116406772740908f3793ef
Suraj Kumar <surajk2072003@gmail.com>
2025-09-17 21:17:33 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,17 +1,17 @@
-a = int(input("Enter First Number : "))
-b = int(input("Enter Second Number : "))
-list =['add','sub','multiply','divide']
+a = int(input("Enter First Number :- "))
+b = int(input("Enter Second Number :- "))
+list =['addition','subtraction','multiplication','divide']
 c = input("Choose  operation : ")
 
 for i in list:
     if (i == c):
-        if(i == 'add'):
+        if(i == 'addition'):
             print(a+b)
             break
-        elif(i == 'sub'):
+        elif(i == 'subtraction'):
             print(a-b)
             break
-        elif(i == 'multiply'):
+        elif(i == 'multiplication'):
             print(a*b)
             break
         else:
